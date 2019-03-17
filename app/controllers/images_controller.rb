6ff0a0c3fbf708aa9836ba11fe5f84890736d702
Abhi28 <abhi.developer28@gmail.com>
2019-03-17 16:17:44 +0530

    Added image resize action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,8 @@
 class ImagesController < ApplicationController
 
-	#Index action, images gets listed in the order at which they were created
+  before_action :set_image, only: [:edit, :destroy, :resize]
+	
+  #Index action, images gets listed in the order at which they were created
 	def index
 	  @images = Image.order('created_at')
 	end
@@ -23,7 +25,6 @@ class ImagesController < ApplicationController
 	end
 
 	def destroy
-		@image = Image.find(params[:id])
 		if @image.destroy
 			flash[:notice] = "Successfully deleted image!"
 			redirect_to root_path
@@ -32,10 +33,24 @@ class ImagesController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def resize
+    ImageUtil.resize_uploaded_image(@image, params[:width].to_i, params[:height].to_i)
+    flash[:notice] = "Successfully resized image!"
+    redirect_to root_path
+	end
+
 	private
 
 	#Permitted parameters when creating a image. This is used for security reasons.
 	def image_params
 	  params.require(:image).permit(:title, :image)
 	end
+
+  # set image from image params
+  def set_image
+    @image = Image.find(params[:id])
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV['AWS_S3_REGION'],
+    s3_protocol: 'https',
+    s3_credentials: {
+      s3_host_name: ENV['AWS_S3_HOSTNAME'],
+      bucket: ENV['AWS_S3_BUCKET'],
+      access_key_id: ENV['AWS_S3_ACCESS_KEY'],
+      secret_access_key: ENV['AWS_S3_SECRET']
+    }
+  }
 end
